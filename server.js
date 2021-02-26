fafb0c9e6a382c9d37df8e05e066402118fa6028
Locullus <bula.mickael@gmail.com>
2021-02-26 22:15:02 +0100

    second commit local après la déclaration des variables d'environnement Lien vers github établi

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+// le path du fichier des variables d'environnement doit être spécifié ici
+require('dotenv').config({path: './config/.env'});
 const app= express();
 
-app.listen(5000, () => console.log("Server is running on port: 5000"));
+app.listen(process.env.PORT, () => console.log(`Server is running on port: ${process.env.PORT}`));
